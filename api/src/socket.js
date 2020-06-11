e0755ac0fec35c49b3f16f0d0782e9530dfd82c8
Clement Gournay <clement.gournay@fastcoding.jp>
2020-06-11 19:26:22 +0200

    Added turn by turn

--- api/src/socket.js
+++ api/src/socket.js
@@ -11,6 +11,8 @@ module.exports = function (server) {
     this.players = {};
     this.messages = [];
     this.playerCount = 0;
+    this.playerIndex = 0;
+    this.currentPlayerID = null;
 
     this.findInArray = (arr, key, value) => {
         let i = 0, found = false;
@@ -24,98 +26,111 @@ module.exports = function (server) {
     this.findPlayerByControlID = (controlID) => {
         let i = 0, found = false;
         let id = null;
-        while (!found && i < Object.keys(this.players).length) {
-            id = Object.keys(this.players)[i];
-            if (this.players[id].controlID === controlID) found = true;
+        while (!found && i < Object.keys(this.players[roomID]).length) {
+            id = Object.keys(this.players[roomID])[i];
+            if (this.players[roomID][id].controlID === controlID) found = true;
             else i++;
         }
-        return (found) ? this.players[id] : null;
+        return (found) ? this.players[roomID][id] : null;
     }
 
     io.on('connection', (socket) => {
     
         const data = socket.request;
 
-        socket.join(roomID);
+        if (!this.players[roomID]) this.players[roomID] = {};
 
-        if (data._query.controlID) {
-            const controlID = data._query.controlID;
-            const player = this.findPlayerByControlID(controlID);
-            console.log('CONTROLLER CONNECTED: ' + controlID);
-            if (player) {
-                console.log('PLAYER FOUND')
-                socket.on('control send', (controlData) => {
-                    console.log('CONTROL SENT', controlData)
-                    io.to(player.socketID).emit('control sent', controlData);
-                });
-            }
+        if (Object.keys(this.players[roomID]).length === 4) {
+            socket.emit('room full');
         } else {
+            socket.join(roomID);
 
-            const id = data._query.id;
-            const name = data._query.name;
-            const position = data._query.position.split(',');
-            const controlID = uuid.v1();
+            if (data._query.controlID) {
+                const controlID = data._query.controlID;
+                const player = this.findPlayerByControlID(controlID);
+                console.log('CONTROLLER CONNECTED: ' + controlID);
+                if (player) {
+                    console.log('PLAYER FOUND')
+                    socket.on('control send', (controlData) => {
+                        console.log('CONTROL SENT', controlData)
+                        io.to(player.socketID).emit('control sent', controlData);
+                    });
+                }
+            } else {
+                const id = data._query.id;
+                const name = data._query.name;
+                const position = data._query.position.split(',');
+                const controlID = uuid.v1();
 
-            this.players[id] = {
-                socketID: socket.id,
-                name: name,
-                position: {block: parseFloat(position[0]), way: parseFloat(position[1]), case: parseFloat(position[2])},
-                controlID: controlID,
-                order: this.playerCount
-            };
-
-            this.playerCount++;
-
-            console.log(name + '(' + socket.id + ') joined the room ' + roomID);
-            socket.emit('players', this.players);
-            socket.emit('controlID', controlID);
-
-            socket.broadcast.to(roomID).emit('player logged in', {id: id, player: this.players[id], order: this.playerOrder});
-
-            socket.emit('chat messages', this.messages);
-
-            socket.on('way chose', (way) => {
-                socket.broadcast.to(roomID).emit('way chosen', {id: id, way: way});
-            });
-
-            socket.on('hit dice', (score) => {
-                socket.broadcast.to(roomID).emit('dice hit', {id: id, score: score});
-            });
-
-            socket.on('chat send', (msg) => {
-                const message = {
-                    id: uuid.v1(),
-                    tempID: msg.tempID,
-                    author: {id: id, name: this.players[id].name},
-                    date: new Date(),
-                    content: msg.content
+                this.players[roomID][id] = {
+                    socketID: socket.id,
+                    name: name,
+                    position: {block: parseFloat(position[0]), way: parseFloat(position[1]), case: parseFloat(position[2])},
+                    controlID: controlID,
+                    order: this.playerCount
                 };
+                if (this.playerCount === 0) this.currentPlayerID = id;
 
-                /*Message.create({
-                    authorID: id,
-                    roomID: '',
-                    content: msg.content
-                }, function(error) {
-                    if(error) {
-                        console.log('error on save');
-                    }
-                });*/
+                this.playerCount++;
 
-                console.log(message);
-                this.messages.push(message);
-                io.to(roomID).emit('chat sent', message);
-            });
+                console.log(name + '(' + socket.id + ') joined the room ' + roomID);
+                socket.emit('players', {players: this.players[roomID], currentPlayerID: this.currentPlayerID});
+                socket.emit('controlID', controlID);
 
-            socket.on('disconnect', () => {
-                delete this.players[id];
-                //const mainVoiceRoom = voiceRoom.getMainRoom();
-                //mainVoiceRoom.removePeer(id);
-                socket.leave(roomID);
-                this.playerCount--;
-                console.log(name +  '(' + socket.id + ') disconnected from room ' + roomID);
-                socket.broadcast.to(roomID).emit('player left', {id: id, order: this.playerOrder});
-                console.log('USER LIST', this.players);
-            });    
+                socket.broadcast.to(roomID).emit('player logged in', {id: id, player: this.players[roomID][id]});
+
+                socket.emit('chat messages', this.messages);
+
+                socket.on('way chose', (way) => {
+                    socket.broadcast.to(roomID).emit('way chosen', {id: id, way: way});
+                });
+
+                socket.on('hit dice', (score) => {
+                    socket.broadcast.to(roomID).emit('dice hit', {id: id, score: score});
+                });
+
+                socket.on('player turn over', () => {
+                    this.playerIndex++;
+                    if (this.playerIndex > Object.keys(this.players[roomID]).length - 1) this.playerIndex = 0;
+                    this.currentPlayerID = Object.keys(this.players[roomID])[this.playerIndex];
+                    io.to(roomID).emit('next player turn', this.currentPlayerID);
+                });
+
+                socket.on('chat send', (msg) => {
+                    const message = {
+                        id: uuid.v1(),
+                        tempID: msg.tempID,
+                        author: {id: id, name: this.players[roomID][id].name},
+                        date: new Date(),
+                        content: msg.content
+                    };
+
+                    /*Message.create({
+                        authorID: id,
+                        roomID: '',
+                        content: msg.content
+                    }, function(error) {
+                        if(error) {
+                            console.log('error on save');
+                        }
+                    });*/
+
+                    console.log(message);
+                    this.messages.push(message);
+                    io.to(roomID).emit('chat sent', message);
+                });
+
+                socket.on('disconnect', () => {
+                    delete this.players[roomID][id];
+                    //const mainVoiceRoom = voiceRoom.getMainRoom();
+                    //mainVoiceRoom.removePeer(id);
+                    socket.leave(roomID);
+                    this.playerCount--;
+                    console.log(name +  '(' + socket.id + ') disconnected from room ' + roomID);
+                    socket.broadcast.to(roomID).emit('player left', id);
+                    console.log('USER LIST', this.players[roomID]);
+                });    
+            }
         }
 
     });
